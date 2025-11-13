6167c2b4ecb4558e2ae93fc215368a6e852213f5
taejin <jinah0206@naver.com>
2025-11-13 21:56:55 +0900

    refactor:  불필요한 소스 제거 GlobalExceptionHandler.java GlobalResponseWrapper.java ResponseMessage.java

--- apps/admin/src/main/java/com/farm2pot/controller/AdminController.java
+++ apps/admin/src/main/java/com/farm2pot/controller/AdminController.java
@@ -1,11 +1,7 @@
 package com.farm2pot.controller;
 
-import com.farm2pot.common.dto.ResponseMessage;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/admin")
--- common/src/main/java/com/farm2pot/common/advice/GlobalResponseWrapper.java
+++ common/src/main/java/com/farm2pot/common/advice/GlobalResponseWrapper.java
@@ -1,50 +1,50 @@
-package com.farm2pot.common.advice;
-
-import com.farm2pot.common.dto.ResponseMessage;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.core.MethodParameter;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
-
-/**
- * packageName    : com.farm2pot.common.response
- * author         : TAEJIN
- * date           : 2025-11-08
- * description    :
- */
-
-@ControllerAdvice(basePackages = "com.farm2pot")
-@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
-public class GlobalResponseWrapper implements ResponseBodyAdvice<Object> {
-
-    @Override
-    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
-        return true;
-    }
-
-    @Override
-    public Object beforeBodyWrite(Object body,
-                                  MethodParameter returnType,
-                                  MediaType selectedContentType,
-                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
-                                  ServerHttpRequest request,
-                                  ServerHttpResponse response) {
-
-        // 이미 ResponseMessage 타입이면 그대로 반환
-        if (body instanceof ResponseMessage) {
-            return body;
-        }
-
-        // void 메서드 처리 (body == null)
-        if (body == null) {
-            return ResponseMessage.success("OK", null);
-        }
-
-        // 기본 success wrapper 적용
-        return ResponseMessage.success("Success", body);
-    }
-}
+//package com.farm2pot.common.advice;
+//
+//import com.farm2pot.common.dto.ResponseMessage;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+//import org.springframework.core.MethodParameter;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.converter.HttpMessageConverter;
+//import org.springframework.http.server.ServerHttpRequest;
+//import org.springframework.http.server.ServerHttpResponse;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+//
+///**
+// * packageName    : com.farm2pot.common.response
+// * author         : TAEJIN
+// * date           : 2025-11-08
+// * description    :
+// */
+//
+//@ControllerAdvice(basePackages = "com.farm2pot")
+//@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+//public class GlobalResponseWrapper implements ResponseBodyAdvice<Object> {
+//
+//    @Override
+//    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+//        return true;
+//    }
+//
+//    @Override
+//    public Object beforeBodyWrite(Object body,
+//                                  MethodParameter returnType,
+//                                  MediaType selectedContentType,
+//                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+//                                  ServerHttpRequest request,
+//                                  ServerHttpResponse response) {
+//
+//        // 이미 ResponseMessage 타입이면 그대로 반환
+//        if (body instanceof ResponseMessage) {
+//            return body;
+//        }
+//
+//        // void 메서드 처리 (body == null)
+//        if (body == null) {
+//            return ResponseMessage.success("OK", null);
+//        }
+//
+//        // 기본 success wrapper 적용
+//        return ResponseMessage.success("Success", body);
+//    }
+//}

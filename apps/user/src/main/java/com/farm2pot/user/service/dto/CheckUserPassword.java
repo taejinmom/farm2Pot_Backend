05527f49ed7b51b829a03f1911e2be10ac5d4995
taejin <jinah0206@naver.com>
2025-11-25 23:15:46 +0900

    refactor: 클래스 명 변경

--- apps/user/src/main/java/com/farm2pot/user/service/dto/CheckUserPassword.java
+++ apps/user/src/main/java/com/farm2pot/user/service/dto/CheckUserPassword.java
@@ -3,7 +3,7 @@ package com.farm2pot.user.service.dto;
 import lombok.*;
 
 @Builder(toBuilder = true)
-public record UserPasswordCheckDto (
+public record CheckUserPassword(
     Long id,
     String password,   // request에 있는 password
     String loginId,
